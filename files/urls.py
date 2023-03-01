99a1fb2823f4f6c69546e97f20f676d76040885d
Martini310 <martin310@wp.pl>
2023-03-01 21:34:19 +0100

    Created views and templates for Order and Vehicle details

--- files/urls.py
+++ files/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('list/', views.list_cars, name='list'),
     path('add/', views.AddVehicle.as_view(), name='add'), 
     path('order/', views.AddOrder.as_view(), name='order'), 
-    path('my_order/', views.my_order, name='my_order')
+    path('my_order/', views.my_order, name='my_order'), 
+    path('orders/', views.Orders.as_view(), name='orders'), 
+    path('order_details/<int:pk>/', views.OrderDetails.as_view(), name='order_details'), 
+    path('update/<int:pk>/', views.VehicleUpdateView.as_view(), name='update_vehicle')
 ]
