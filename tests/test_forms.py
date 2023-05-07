5b769f9564cb996c15b6c9e7217aa174c16c6656
Martini310 <martin310@wp.pl>
2023-05-07 23:01:54 +0200

    test: Add new test for TransferForm

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,35 +1,48 @@
-from django.test import TestCase
-from files.forms import MyOrderForm
+from django.test import TestCase, Client
+from files.forms import MyOrderForm, TransferForm
 from files.models import Vehicle, User
 # Create your tests here.
 
 class MyOrderFormTest(TestCase):
         
-        def test_file_is_taken(self):
-            tr = 'PZ 12345'
-            user = User.objects.create(username='test')
-            Vehicle.objects.create(tr=tr, responsible_person=user, status='o')
-            form = MyOrderForm(data={'tr': tr})
-            self.assertFalse(form.is_valid())
+    def test_file_is_taken(self):
+        tr = 'PZ 12345'
+        user = User.objects.create(username='test')
+        Vehicle.objects.create(tr=tr, responsible_person=user, status='o')
+        form = MyOrderForm(data={'tr': tr})
+        self.assertFalse(form.is_valid())
 
-        def test_file_is_returned(self):
-            tr = 'PZ 12345'
-            user = User.objects.create(username='test')
-            Vehicle.objects.create(tr=tr, responsible_person=user, status='r')
-            form = MyOrderForm(data={'tr': tr})
-            self.assertTrue(form.is_valid())
+    def test_file_is_returned(self):
+        tr = 'PZ 12345'
+        user = User.objects.create(username='test')
+        Vehicle.objects.create(tr=tr, responsible_person=user, status='r')
+        form = MyOrderForm(data={'tr': tr})
+        self.assertTrue(form.is_valid())
 
-        def test_file_is_rejected(self):
-            tr = 'PZ 12345'
-            user = User.objects.create(username='test')
-            Vehicle.objects.create(tr=tr, responsible_person=user, status='e')
-            form = MyOrderForm(data={'tr': tr})
-            self.assertTrue(form.is_valid())
+    def test_file_is_rejected(self):
+        tr = 'PZ 12345'
+        user = User.objects.create(username='test')
+        Vehicle.objects.create(tr=tr, responsible_person=user, status='e')
+        form = MyOrderForm(data={'tr': tr})
+        self.assertTrue(form.is_valid())
 
-        def test_file_is_ordered(self):
-            tr = 'PZ 12345'
-            user = User.objects.create(username='test')
-            Vehicle.objects.create(tr=tr, responsible_person=user, status='a')
-            form = MyOrderForm(data={'tr': tr})
-            self.assertFalse(form.is_valid())
-            
+    def test_file_is_ordered(self):
+        tr = 'PZ 12345'
+        user = User.objects.create(username='test')
+        Vehicle.objects.create(tr=tr, responsible_person=user, status='a')
+        form = MyOrderForm(data={'tr': tr})
+        self.assertFalse(form.is_valid())
+        
+class TransferFormTest(TestCase):
+
+    def test_transfer_to_another_user(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.user2 = User.objects.create_user(username='testuser2', password='testpass')
+
+        self.client.login(username='testuser', password='testpass')
+        # user = User.objects.create(username='test')
+        # user2 = User.objects.create(username='test2')
+        Vehicle.objects.create(tr='AA 12345', responsible_person=self.user, status='o')
+        form = TransferForm(data={'transfering_to': self.user2})
+        self.assertTrue(form.is_valid())
