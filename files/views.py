7be993e03456ee5f1b2d6a777bb8ad6365be17d2
Martini310 <martin310@wp.pl>
2023-05-11 22:21:36 +0200

    feat(home): Add cards to select application
    There will be more applications in future, so home view is now ready to handle it

--- files/views.py
+++ files/views.py
@@ -5,7 +5,6 @@ from django.views.generic import ListView, CreateView, UpdateView, FormView, Tem
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import ReturnForm, MyOrderFormSet, TransferForm, AddVehicleForm
