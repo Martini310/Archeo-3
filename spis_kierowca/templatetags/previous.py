31078e4fd9431ecb4b7763a432cdf6fb5b6ed224
Martini310 <martin310@wp.pl>
2023-05-30 22:14:26 +0200

    feat(spis): Add header at the begining of  every transfer_list in search view

---
+++ spis_kierowca/templatetags/previous.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def previous(some_list, current_index):
+    """
+    Returns the previous element of the list using the current index if it exists.
+    Otherwise returns an empty string.
+    """
+    try:
+        return some_list[int(current_index) - 1] # access the previous element
+    except:
+        return '' # return empty string in case of exception
